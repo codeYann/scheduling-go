5694b6002fdfd280590c53f17d3c0a12d5797ccc
codeYann <ccyan05@alu.ufc.br>
2022-06-12 13:59:04 -0300

    adding time and getters funcions

--- process/process.go
+++ process/process.go
@@ -4,18 +4,29 @@ import (
 	"math/rand"
 )
 
+// Create Pid type
 type Pid int
 
+// Create process type
 type Process struct {
 	processId Pid
+	time      int
 }
 
-func (p *Process) CreateProcess() *Process {
+// Create process function
+func CreateProcess() *Process {
 	return &Process{
-		processId: Pid(rand.Int()),
+		processId: Pid(rand.Intn(100)),
+		time:      rand.Intn(100) / 1000,
 	}
 }
 
-func (p Process) GetPid() Pid {
+// GetProcessId function
+func (p *Process) GetProcessId() Pid {
 	return p.processId
+}
+
+// GetTime function
+func (p *Process) GetTime() int {
+	return p.time
 }
